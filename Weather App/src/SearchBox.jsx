187f8fb617a7804f17ee1d74874843a2631f9c79
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-08 21:36:47 +0530

    Using weather API

--- Weather App/src/SearchBox.jsx
+++ Weather App/src/SearchBox.jsx
@@ -6,19 +6,40 @@ import { IconButton } from "@mui/material";
 import "./SearchBox.css";
 import { useState } from "react";
 
-
 export default function SearchBox() {
   let [city, setCity] = useState("");
 
+  let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "313f9a1717c75955e6effe713ccce49f";
+
+  let getWeatherInfo = async (city) => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+
+    let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_like,
+        weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
+
   let handleChange = (event) => {
     setCity(event.target.value);
-  }
+  };
 
   let handleSubmit = (event) => {
     event.preventDefault();
     console.log(city);
     setCity("");
-  }
+    getWeatherInfo(city);
+  };
 
   return (
     <div>
@@ -29,7 +50,8 @@ export default function SearchBox() {
             id="city"
             label="City or Airport"
             variant="outlined"
-            required value={city}
+            required
+            value={city}
             onChange={handleChange}
             InputProps={{
               endAdornment: (
